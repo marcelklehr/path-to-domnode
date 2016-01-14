577af7aa4ae26e8537d75e12585e5935652ac08f
Marcel Klehr <mklehr@gmx.net>
2016-01-14 15:21:44 +0100

    let there be :bulb:

---
+++ README.md
@@ -0,0 +1,12 @@
+# path to DOMNode
+## pathTo(path:Array, rootNode:DOMNode)
+* `path` is an array, where `[]` is the root node, `[0]` is the first child node and `[0,1]` is the second child of the first child of the root node.
+* `rootNode` is the reference in respect to which the paths are resolved
+
+## See also
+* [DOMNOde at path](https://github.com/marcelklehr/domnode-at-path)
+
+## Legal
+(c) 2016 by Marcel Klehr
+
+MIT-License
---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * path-to-domnode
+ * Copyright (C) 2016 Marcel Klehr <mklehr@gmx.net>
+ */
+module.exports = function pathTo(node, root) {
+  if(!root) throw new Error('No root node specified.')
+
+  if(node === root) return []
+
+  if(!root.contains(node)) {
+    throw new Error('Cannot determine path. Node is not a descendant of root node.')
+  }
+
+  // The number of older siblings equals my index in the list of childNodes
+  var myIndex = 0, n = node
+  while(n.previousSibling) {
+    n = n.previousSibling
+    myIndex++
+  }
+
+  var parentPath = pathTo(node.parentNode, root)
+  parentPath.push(myIndex)
+
+  return parentPath
+}
